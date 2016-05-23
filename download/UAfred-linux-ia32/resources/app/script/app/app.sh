224cdc5c2b46c0d6f57045ff1d175fe3eddac9e0
zhenyangze <zhenyangze@gmail.com>
2016-05-23 19:38:04 +0800

    ignore cache

--- download/UAfred-linux-ia32/resources/app/script/app/app.sh
+++ download/UAfred-linux-ia32/resources/app/script/app/app.sh
@@ -9,31 +9,49 @@ if [[ -z $1 ]];then
 fi
 KEYWORD=$1
 NUM=0
+temp_fifo_file=$$.info        #以当前进程号，为临时管道取名  
+mkfifo $temp_fifo_file        #创建临时管道  
+exec 6<>$temp_fifo_file       #创建标识为6，可以对管道进行读写  
+rm $temp_fifo_file            #清空管道内容  
+
+temp_thread=2                 #进程数  
+
+for ((i=0;i<10;i++)) #为进程创建相应的占位  
+do  
+    echo >&6                        #每个echo输出一个回车，为每个进程创建一个占位  
+done                     #将占位信息写入标识为6的管道
+
 for i in $(ls $APP_PATH);do
-    FILE_PATH=$APP_PATH/$i
-    COUNT=$(cat $FILE_PATH | grep -E 'Name|Comment|Keywords' | grep -i $KEYWORD | wc -l)
-    if [[ $COUNT -gt 0 ]];then
-        Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
-        Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
-        if [[ ! -z $Name_ZH ]];then
-            Name=$Name_ZH
+    read -u6
+    {
+        FILE_PATH=$APP_PATH/$i
+        COUNT=$(cat $FILE_PATH | grep -E 'Name|Comment|Keywords' | grep -i $KEYWORD | wc -l)
+        if [[ $COUNT -gt 0 ]];then
+            Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
+            Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
+            if [[ ! -z $Name_ZH ]];then
+                Name=$Name_ZH
+            fi
+            Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
+            Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
+            if [[ ! -z $Comment_ZH ]];then
+                Comment=$Comment_ZH
+            fi
+            Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
+            Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
+            REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
+            if [[ ! -f $REAL_ICON ]];then
+                REAL_ICON=''
+            fi
+            RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
+            echo $RESULT
+            NUM=`expr $NUM + 1`
+            if [[ $NUM -gt 50 ]];then
+                exit
+            fi
         fi
-        Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
-        Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
-        if [[ ! -z $Comment_ZH ]];then
-            Comment=$Comment_ZH
-        fi
-        Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
-        Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
-        REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
-        if [[ ! -f $REAL_ICON ]];then
-            REAL_ICON=''
-        fi
-        RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
-        echo $RESULT
-        NUM=`expr $NUM + 1`
-        if [[ $NUM -gt 12 ]];then
-            exit
-        fi
-    fi
+        echo >&6
+    }&
+    wait
 done
+exec 6>&- 
--- download/UAfred-linux-x64/resources/app/README.md
+++ download/UAfred-linux-x64/resources/app/README.md
@@ -2,22 +2,23 @@
 afred on ubuntu(use electron)
 **ubuntu**上的**afred**
 
-#　使用方法
+使用方法
+----
 1. 进入　[下载地址](https://github.com/zhenyangze/uafred/tree/master/download)，选择自己的系统对应的版本下载．
 2. 创建快捷方式
 　右上角系统设置->键盘->快捷键->自定义
 3. 使用
+  * 使用快捷键打开程序
+  * `Esc`关闭程序
+  * `Alt+数字`执行对应的方法（有些会失效，比如有道翻译，因为没有对Alt做处理）
+  * `Enter`则执行第一个命令
+  * 注意，在对应目录中，可执行文件是**UAfred**，命令行运行`./your/path/UAfred`进行测试
+  * 不要问为啥alfred改成了uafred,他们之间差了个lu而已
 
-> * 使用快捷键打开程序
-> * `Esc`关闭程序
-> * `Alt+数字`执行对应的方法（有些会失效，比如有道翻译，因为没有对Alt做处理）
-> * `Enter`则执行第一个命令
-> * 注意，在对应目录中，可执行文件是**UAfred**，命令行运行`./your/path/UAfred`进行测试
-> * 不要问为啥alfred改成了uafred,他们之间差了个lu而已
-
-#　主要功能
+主要功能
+----
 ## 帮助
-![应用搜索](https://github.com/zhenyangze/uafred/raw/master/screenshot/help.png)
+![帮助搜索](https://github.com/zhenyangze/uafred/raw/master/screenshot/help.png)
 ## 应用搜索
 ![应用搜索](https://github.com/zhenyangze/uafred/raw/master/screenshot/app.png)
 ## 有道翻译
