59e26495013f91c2e0be0630db312813307d56ee
zhenyangze <zhenyangze@gmail.com>
2016-06-24 11:54:00 +0800

    add tmpfile to app

--- download/UAfred-linux-ia32/resources/app/script/app/app.sh
+++ download/UAfred-linux-ia32/resources/app/script/app/app.sh
@@ -9,31 +9,38 @@ if [[ -z $1 ]];then
 fi
 KEYWORD=$1
 NUM=0
-for i in $(ls $APP_PATH);do
-    FILE_PATH=$APP_PATH/$i
-    COUNT=$(cat $FILE_PATH | grep -E 'Name|Comment|Keywords' | grep -i $KEYWORD | wc -l)
-    if [[ $COUNT -gt 0 ]];then
-        Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
-        Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
-        if [[ ! -z $Name_ZH ]];then
-            Name=$Name_ZH
-        fi
-        Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
-        Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
-        if [[ ! -z $Comment_ZH ]];then
-            Comment=$Comment_ZH
-        fi
-        Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
-        Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
-        REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
-        if [[ ! -f $REAL_ICON ]];then
-            REAL_ICON=''
-        fi
-        RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
-        echo $RESULT
-        NUM=`expr $NUM + 1`
-        if [[ $NUM -gt 50 ]];then
-            exit
-        fi
+
+tmpFile=$(ls /tmp/uafred_${KEYWORD}.* 2>/dev/null | head -1)
+if [[ ! -z $tmpFile ]];then
+    cat $tmpFile
+    exit
+else
+    tmpFile=$(mktemp /tmp/uafred_${KEYWORD}.XXXX)
+fi
+
+for i in $(grep -i $KEYWORD $APP_PATH/* | grep -E 'Name|Comment|Keywords' | awk -F':' '{print $1}' | uniq);do
+    FILE_PATH=$i
+    Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
+    Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
+    if [[ ! -z $Name_ZH ]];then
+        Name=$Name_ZH
+    fi
+    Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
+    Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
+    if [[ ! -z $Comment_ZH ]];then
+        Comment=$Comment_ZH
+    fi
+    Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
+    Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
+    REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
+    if [[ ! -f $REAL_ICON ]];then
+        REAL_ICON=''
+    fi
+    RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
+    echo $RESULT
+    echo $RESULT >> $tmpFile
+    NUM=`expr $NUM + 1`
+    if [[ $NUM -gt 50 ]];then
+        exit
     fi
 done
---
+++ download/UAfred-linux-x64/resources/app/script/find/find.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# File Name: find.sh
+# Author: zhenyangze
+# mail: zhenyangze@51talk.com
+# Created Time: 2016年05月31日 星期二 12时01分28秒
+if [[ -z $1 ]];then
+    fileName="*";
+else
+    fileName=$1
+fi
