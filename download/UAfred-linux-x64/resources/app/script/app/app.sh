237a6b0988415331d011344cee3515974b025288
zhenyangze <zhenyangze@gmail.com>
2016-06-25 19:42:35 +0800

    新增app路径

--- download/UAfred-linux-x64/resources/app/script/app/app.sh
+++ download/UAfred-linux-x64/resources/app/script/app/app.sh
@@ -3,7 +3,8 @@
 # Author: zhenyangze
 # mail: zhenyangze@gmail.com
 
-APP_PATH='/usr/share/applications'
+APP_PATH_ARRAY=('/usr/share/applications' $HOME'/.local/share/applications')
+
 if [[ -z $1 ]];then
     exit
 fi
@@ -18,29 +19,31 @@ else
     tmpFile=$(mktemp /tmp/uafred_${KEYWORD}.XXXX)
 fi
 
-for i in $(grep -i $KEYWORD $APP_PATH/* | grep -E 'Name|Comment|Keywords' | awk -F':' '{print $1}' | uniq);do
-    FILE_PATH=$i
-    Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
-    Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
-    if [[ ! -z $Name_ZH ]];then
-        Name=$Name_ZH
-    fi
-    Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
-    Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
-    if [[ ! -z $Comment_ZH ]];then
-        Comment=$Comment_ZH
-    fi
-    Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
-    Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
-    REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
-    if [[ ! -f $REAL_ICON ]];then
-        REAL_ICON=''
-    fi
-    RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
-    echo $RESULT
-    echo $RESULT >> $tmpFile
-    NUM=`expr $NUM + 1`
-    if [[ $NUM -gt 50 ]];then
-        exit
-    fi
+for APP_PATH in ${APP_PATH_ARRAY[*]};do
+    for i in $(grep -i $KEYWORD $APP_PATH/* | grep -E 'Name|Comment|Keywords' | awk -F':' '{print $1}' | uniq);do
+        FILE_PATH=$i
+        Name=$(cat $FILE_PATH | grep 'Name=' | head -1 | sed -n 's/Name=//p') 
+        Name_ZH=$(cat $FILE_PATH | grep 'Name\[zh_CN\]=' | head -1 | sed -n 's/Name\[zh_CN\]=//p') 
+        if [[ ! -z $Name_ZH ]];then
+            Name=$Name_ZH
+        fi
+        Comment=$(cat $FILE_PATH | grep 'Comment=' | head -1 | sed -n 's/Comment=//p') 
+        Comment_ZH=$(cat $FILE_PATH | grep 'Comment\[zh_CN\]=' | head -1 | sed -n 's/Comment\[zh_CN\]=//p') 
+        if [[ ! -z $Comment_ZH ]];then
+            Comment=$Comment_ZH
+        fi
+        Exec=$(cat $FILE_PATH | grep 'Exec=' | head -1 | sed -n 's/Exec=//p' | cut -d ' ' -f 1) 
+        Icon=$(cat $FILE_PATH | grep 'Icon=' | head -1 | sed -n 's/Icon=//p') 
+        REAL_ICON=$(tree -fin /usr/share/icons/hicolor | grep '\/'$Icon | head -1 | awk '{print $1}')
+        if [[ ! -f $REAL_ICON ]];then
+            REAL_ICON=''
+        fi
+        RESULT='{"Key":"'$KEYWORD'","Name": "'$Name'","Comment":"'$Comment'","Exec":"'$Exec'","Icon":"'$REAL_ICON'"}'
+        echo $RESULT
+        echo $RESULT >> $tmpFile
+        NUM=`expr $NUM + 1`
+        if [[ $NUM -gt 50 ]];then
+            exit
+        fi
+    done
 done
