d3370567884001b0f58d1121532aa63821410cf3
zhenyangze <zhenyangze@gmail.com>
2016-08-19 14:55:49 +0800

    添加豆瓣电影 by YYRancho

--- download/UAfred-linux-x64/resources/app/app/script/youdao/youdao.py
+++ download/UAfred-linux-x64/resources/app/app/script/youdao/youdao.py
@@ -3,6 +3,8 @@
 
 # http://fanyi.youdao.com/openapi.do?keyfrom=taohaoge&key=527038574&type=data&doctype=json&version=1.1&q=
 
+import re
+import urllib
 import urllib2
 import json
 import sys
@@ -11,7 +13,8 @@ sys.setdefaultencoding('utf-8')
 
 # text command icon subtext
 
-keyword = '+'.join(sys.argv[1:])
+keyword = ' '.join(sys.argv[1:])
+keyword = urllib.quote(keyword)
 if len(keyword) == 0:
     exit()
 
@@ -23,14 +26,17 @@ response = urllib2.Request(url)
 browser = "Mozilla/5.0 (Windows NT 6.1; WOW64)"
 response.add_header('User-Agent', browser)
 response = urllib2.urlopen(response)
-get_page = response.read()
-get_page = json.loads(get_page)
+get_page_raw = response.read()
+get_page = json.loads(get_page_raw)
 
 if get_page['errorCode'] != 0:
     exit()
 
 
-print '{"Name":"' + ' '.join(get_page['translation']).strip() + '","Icon":"../../script/youdao/youdao.png","Exec":"","Comment":"' + get_page['query'] + '"}'
+if re.search('us-phonetic', get_page_raw):
+	print '{"Name":"' + ' '.join(get_page['translation']).strip() + '","Icon":"../../script/youdao/youdao.png","Exec":"","Comment":"' + get_page['query'] + ' US: [' + get_page['basic']['us-phonetic'] + ']' + ' UK: [' + get_page['basic']['uk-phonetic'] + ']' '"}'
+else:	
+	print '{"Name":"' + ' '.join(get_page['translation']).strip() + '","Icon":"../../script/youdao/youdao.png","Exec":"","Comment":"' + get_page['query'] + '"}'
 
 
 for i in get_page['web']:
